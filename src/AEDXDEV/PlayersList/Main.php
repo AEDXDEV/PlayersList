de94473f4b0b57406c08ceea80a54ba8d5603f1d
AEDXDEV <103389504+AEDXDEV@users.noreply.github.com>
2024-05-18 02:02:12 +0300

    Update Main.php

--- src/AEDXDEV/PlayersList/Main.php
+++ src/AEDXDEV/PlayersList/Main.php
@@ -1,12 +1,31 @@
 <?php
 
-/*
-by AEDXDV
+/**
+  *  A free plugin for PocketMine-MP.
+  *	
+  *	Copyright (c) AEDXDEV
+  *  
+  *	Youtube: AEDX DEV 
+  *	Discord: aedxdev
+  *	Github: AEDXDEV
+  *	Email: aedxdev@gmail.com
+  *	Donate: https://paypal.me/AEDXDEV
+  *   
+  *        This program is free software: you can redistribute it and/or modify
+  *        it under the terms of the GNU General Public License as published by
+  *        the Free Software Foundation, either version 3 of the License, or
+  *        (at your option) any later version.
+  *
+  *        This program is distributed in the hope that it will be useful,
+  *        but WITHOUT ANY WARRANTY; without even the implied warranty of
+  *        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  *        GNU General Public License for more details.
+  *
+  *        You should have received a copy of the GNU General Public License
+  *        along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  *         
+  */
 
-Youtube: @AEDXDEV
-Discord: aedxdev
-
-*/
 namespace AEDXDEV\PlayersList;
 
 use JaxkDev\DiscordBot\Models\Messages\Embed\Embed;
